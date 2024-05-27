12f951503aaf69bc6290348924e5e14c7f49adc0
Muhmmad Hashim <hashiimtahir@gmail.com>
2024-05-27 22:08:00 +0500

    extracting morgecalculator class

--- 02_object_oriented_programming/02_Refactoring_towards_an_OOP_design/04_extracting_he_morge_report_class/main.py
+++ 02_object_oriented_programming/02_Refactoring_towards_an_OOP_design/04_extracting_he_morge_report_class/main.py
@@ -1,21 +1,49 @@
-import locale
+import math
 
+class MortgageCalculator:
+    MONTHS_IN_YEAR = 12
+    PERCENT = 100
 
-class MortgageReport:
-    def __init__(self, calculator):
-        self.calculator = calculator
-        self.currency = locale.currency
+    def __init__(self, principal, annual_interest, years):
+        self.principal = principal
+        self.annual_interest = annual_interest
+        self.years = years
 
-    def print_payment_schedule(self):
-        print("\nPAYMENT SCHEDULE")
-        print("----------------")
-        for balance in self.calculator.get_remaining_balances():
-            print(self.currency(balance))
+    def calculate_balance(self, number_of_payments_made):
+        monthly_interest = self.get_monthly_interest()
+        number_of_payments = self.get_number_of_payments()
 
-    def print_mortgage(self):
-        mortgage = self.calculator.calculate_mortgage()
-        mortgage_formatted = self.currency(mortgage, grouping=True)
-        print("\nMORTGAGE")
-        print("--------")
-        print("Monthly Payments:", mortgage_formatted)
-        
+        balance = (self.principal * 
+                  (math.pow(1 + monthly_interest, number_of_payments) - 
+                   math.pow(1 + monthly_interest, number_of_payments_made)) /
+                  (math.pow(1 + monthly_interest, number_of_payments) - 1))
+
+        return balance
+
+    def calculate_mortgage(self):
+        monthly_interest = self.get_monthly_interest()
+        number_of_payments = self.get_number_of_payments()
+
+        mortgage = (self.principal * 
+                   (monthly_interest * math.pow(1 + monthly_interest, number_of_payments)) / 
+                   (math.pow(1 + monthly_interest, number_of_payments) - 1))
+
+        return mortgage
+
+    def get_remaining_balances(self):
+        balances = []
+        number_of_payments = self.get_number_of_payments()
+        for month in range(1, number_of_payments + 1):
+            balances.append(self.calculate_balance(month))
+        return balances
+
+    def get_monthly_interest(self):
+        return self.annual_interest / self.PERCENT / self.MONTHS_IN_YEAR
+
+    def get_number_of_payments(self):
+        return self.years * self.MONTHS_IN_YEAR
+
+# Example usage
+# calculator = MortgageCalculator(100000, 5, 30)
+# print(calculator.calculate_mortgage())
+# print(calculator.get_remaining_balances())
