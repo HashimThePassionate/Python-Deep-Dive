978587f61b42394dc57887b88ffa6ce7753b0aba
Muhammad Hashim <hashiimtahir@gmail.com>
2024-06-24 14:01:30 +0500

    Emulating Numeric Types with a Custom Vector Class

---
+++ 06_advance_python_deep_dive/01_data_structure/01_the_data_model/03_emulating_numeric_types/main.py
@@ -0,0 +1,30 @@
+import math
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    
+    def __repr__(self):
+        return f'Vector({self.x!r}, {self.y!r})'
+    
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+    
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+    
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+
+
+
+
+v1 = Vector(2, 4)
+v2 = Vector(2, 1)
+print(v1 + v2)
