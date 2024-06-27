280513fd473a994ca815c21cd80b3811e0798bd9
Muhammad Hashim <hashiimtahir@gmail.com>
2024-06-27 14:12:55 +0500

    Assigning to Slices

---
+++ 06_advance_python_deep_dive/01_data_structure/02_array_of_squences/12_assigning_to_slices/main.py
@@ -0,0 +1,19 @@
+l = list(range(10))
+print(l)  # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+l[2:5] = [20, 30]
+print(l)  # Output: [0, 1, 20, 30, 5, 6, 7, 8, 9]
+
+del l[5:7]
+print(l)  # Output: [0, 1, 20, 30, 5, 8, 9]
+
+l[3::2] = [11, 22]
+print(l)  # Output: [0, 1, 20, 11, 5, 22, 9]
+
+try:
+    l[2:5] = 100
+except TypeError as e:
+    print(e)  # Output: can only assign an iterable
+
+l[2:5] = [100]
+print(l)  # Output: [0, 1, 100, 22, 9]
