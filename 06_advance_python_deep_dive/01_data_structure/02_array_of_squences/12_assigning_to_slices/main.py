50336497be43b1fd788b1e535ae15dbfd8daf76a
Muhammad Hashim <hashiimtahir@gmail.com>
2024-06-27 14:13:37 +0500

    comments in assigning slice

--- 06_advance_python_deep_dive/01_data_structure/02_array_of_squences/12_assigning_to_slices/main.py
+++ 06_advance_python_deep_dive/01_data_structure/02_array_of_squences/12_assigning_to_slices/main.py
@@ -10,10 +10,10 @@ print(l)  # Output: [0, 1, 20, 30, 5, 8, 9]
 l[3::2] = [11, 22]
 print(l)  # Output: [0, 1, 20, 11, 5, 22, 9]
 
-try:
-    l[2:5] = 100
-except TypeError as e:
-    print(e)  # Output: can only assign an iterable
+# try:
+#     l[2:5] = 100
+# except TypeError as e:
+#     print(e)  # Output: can only assign an iterable
 
 l[2:5] = [100]
 print(l)  # Output: [0, 1, 100, 22, 9]
