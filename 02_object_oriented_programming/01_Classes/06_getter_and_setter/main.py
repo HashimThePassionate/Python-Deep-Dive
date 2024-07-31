2919257861bc6dd97517887abd9674e47fa432a2
Muhammad Hashim <89855559+HashimThePassionate@users.noreply.github.com>
2024-07-31 15:24:27 +0500

    Update main.py

--- 02_object_oriented_programming/01_Classes/06_getter_and_setter/main.py
+++ 02_object_oriented_programming/01_Classes/06_getter_and_setter/main.py
@@ -1,28 +1,33 @@
 class Employee:
-    def __init__(self, salary, hours):
-        self.__salary = salary
-        self.__hours = hours
+    def __init__(self, s, h):
+        self.__salary = s
+        self.__hours = h
 
     def calculate_wage(self, extra_hours):
         return f"The Calculated Wage is {self.__salary * self.__hours + 50 * extra_hours}"
 
-    def set_base_salary(self, s):
-        if s <= 0:
-            raise ValueError("Salary must be greater than 0")
-        self.__salary = s
+    def set_salary(self, salary):
+        if salary <= 0:
+            raise ValueError("Salary can not less then or equal to zero")
+        self.__salary = salary
 
-    def get_base_salary(self):
+    def get_salary(self):
         return self.__salary
-    
-    def set_hourly_rate(self, h):
-        if h <0:
-            raise ValueError("Hourly rate cannot be negative")
-        self.__hours = h
-    
-    def get_hourly_rate(self):
+
+    def set_hours(self, hours):
+        if hours <= 0:
+            raise ValueError("Hours can not less then or equal to zero")
+        self.__hours = hours
+
+    def get_hours(self):
         return self.__hours
-
-
-e = Employee(10000,20)
+        
+e = Employee(10000, 20)
 print(e.calculate_wage(10))
-print(e.__salary)
+e.set_salary(2000000.124)
+e.set_hours(30)
+print(e.calculate_wage(10))
+try:
+    print(e.set_salary(0))
+except ValueError as e:
+    print("Salary can not less then or equal to zero")
