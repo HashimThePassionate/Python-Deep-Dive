77273884b84485cff12a6fa98e117d8119be7d32
HashimThePassionate <hashiimtahir@gmail.com>
2025-08-25 23:48:57 -0700

    ⛰ Min heap root delete operation implemented

--- 03_data_structures/05_heaps_and_periority_queues/01_heaps/main.py
+++ 03_data_structures/05_heaps_and_periority_queues/01_heaps/main.py
@@ -15,9 +15,36 @@ class MinHeap:
         self.size += 1
         self.arrange(self.size)
 
+    def minchild(self, k):
+        if k * 2 + 1 > self.size:
+            return k * 2
+        elif self.heap[k * 2] < self.heap[k * 2 + 1]:
+            return k * 2
+        else:
+            return k * 2 + 1
+    
+    def sink(self, k):
+        while k * 2 <= self.size:
+            mc = self.minchild(k)
+            if self.heap[k] > self.heap[mc]:
+                self.heap[k], self.heap[mc] = self.heap[mc], self.heap[k]
+            k = mc
+    
+    def delete_at_root(self):
+        item = self.heap[1]
+        self.heap[1] = self.heap[self.size]
+        self.size -= 1
+        self.heap.pop()
+        self.sink(1)
+        return item
+
 
 h = MinHeap()
 for i in (4, 8, 7, 2, 9, 10, 5, 1, 3, 6):
     h.insert(i)
 
 print(h.heap) 
+
+n = h.delete_at_root()
+print(n)
+print(h.heap)
