d8c619b1e3cfa2729fb2953bded1e84444e34789
HashimThePassionate <hashiimtahir@gmail.com>
2025-08-25 22:35:43 -0700

    ⛰ Min Heap Insertion Implementation

--- 03_data_structures/05_heaps_and_periority_queues/01_heaps/main.py
+++ 03_data_structures/05_heaps_and_periority_queues/01_heaps/main.py
@@ -2,3 +2,22 @@ class MinHeap:
     def __init__(self):
         self.heap = [0]
         self.size = 0
+
+
+    def arrange(self, k):
+        while k // 2 > 0:
+            if self.heap[k] < self.heap[k // 2]:
+                self.heap[k], self.heap[k // 2] = self.heap[k // 2], self.heap[k]
+            k //= 2
+
+    def insert(self, item):
+        self.heap.append(item)
+        self.size += 1
+        self.arrange(self.size)
+
+
+h = MinHeap()
+for i in (4, 8, 7, 2, 9, 10, 5, 1, 3, 6):
+    h.insert(i)
+
+print(h.heap) 
