ffbf648c0cde1a07b6cdce9cf2ba8632bb1d621b
HashimThePassionate <hashiimtahir@gmail.com>
2025-08-29 05:29:59 +0500

    📝 fast hash function

--- 03_data_structures/06_hash_tables/01_Introducing_hash_tables/main.py
+++ 03_data_structures/06_hash_tables/01_Introducing_hash_tables/main.py
@@ -1,7 +1,21 @@
-my_dict = {"Hashim": "9229012345", "Muhammad": "9229012346", "Ishaque":
-           "9229012347", "Sara": "9229012348"}
+# my_dict = {"Hashim": "9229012345", "Muhammad": "9229012346", "Ishaque":
+#            "9229012347", "Sara": "9229012348"}
 
-print("Print All Keys and values")
-for x, y in my_dict.items():
-    print(f"{x}: {y}")
-print(my_dict["Hashim"])
+# print("Print All Keys and values")
+# for x, y in my_dict.items():
+#     print(f"{x}: {y}")
+# print(my_dict["Hashim"])
+
+# print(sum(map(ord,"Hashim")))
+
+def my_hash(s):
+    mult = 1
+    hv = 0
+    for ch in s:
+        hv += ord(ch) * mult
+        mult += 1
+    return hv
+
+for item in ('hello world', 'world hello', 'gello xorld'): 
+    print(f'{item}: {my_hash(item)}')
+
