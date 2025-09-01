a25b96b7fd002ccc4b6aa0d182e98473033c53c6
HashimThePassionate <hashiimtahir@gmail.com>
2025-09-01 06:11:37 +0500

    📝 Check growth and Growth

--- 03_data_structures/06_hash_tables/01_Introducing_hash_tables/hashable.py
+++ 03_data_structures/06_hash_tables/01_Introducing_hash_tables/hashable.py
@@ -9,6 +9,7 @@ class HashTable:
         self.size = 256
         self.slots = [None for i in range(self.size)]
         self.count = 0
+        self.MAXLOADFACTOR = 0.65
 
     def _hash(self, key):
         mult = 1
@@ -17,7 +18,7 @@ class HashTable:
             hv += mult * ord(ch)
             mult += 1
         return hv % self.size
-    
+
     def put(self, key, value):
         item = HashItem(key, value)
         h = self._hash(key)
@@ -30,3 +31,33 @@ class HashTable:
         self.slots[h] = item
         self.check_growth()
 
+    def check_growth(self):
+        loadfactor = self.count / self.size
+        if loadfactor > self.MAXLOADFACTOR:
+            print("Load factor before growing the hash table", self.count
+                  / self.size)
+            self.growth()
+            print("Load factor after growing the hash table", self.count /
+                  self.size)
+
+    def growth(self):
+        New_Hash_Table = HashTable()
+        New_Hash_Table.size = 2 * self.size
+        New_Hash_Table.slots = [None for i in range(New_Hash_Table.size)]
+
+        for i in range(self.size):
+            if self.slots[i] != None:
+                New_Hash_Table.put(self.slots[i].key, self.slots[i].value)
+        self.size = New_Hash_Table.size
+        self.slots = New_Hash_Table.slots
+
+
+ht = HashTable()
+ht.put("good", "eggs")
+ht.put("better", "ham")
+ht.put("best", "spam")
+ht.put("ad", "do not")
+ht.put("ga", "collide")
+ht.put("awd", "do not")
+ht.put("add", "do not")
+ht.check_growth()
